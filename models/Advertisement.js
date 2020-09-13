04b2c8983423939c455058b22e18991a73ac4ba5
Sebastian Perez <sebperez@gmail.com>
2020-09-12 21:07:04 -0300

    feat: logica de filtros al modelo

--- models/Advertisement.js
+++ models/Advertisement.js
@@ -41,7 +41,67 @@ const advertisementSchema = mongoose.Schema({
 }
 );
 
-advertisementSchema.statics.lista = function(filter, limit, skip, sort) {
+advertisementSchema.statics.lista = function(req) { 
+  // recibimos el objeto req desde el router, toda esta lógica la ubiqué en el modelo
+  // para poder reutilizarla cuando es llamada desde la vista index
+  const filter = {};
+  if(req.query.name) {
+    // Uso expresiones regulares para hacer un filtro "like" con la opción 'i' que no sea case sensitive
+    filter.name = new RegExp('^' + req.query.name,'i');
+  }
+
+  if(req.query.sell) {
+    filter.sell = req.query.sell;
+  }
+
+  if(req.query.tag) {
+    filter.tags = { $all: req.query.tag }; //busca 1 o más tags en los anuncios disponibles
+  }
+
+  if(req.query.currency) {
+    filter.currency = req.query.currency.toUpperCase();
+  }
+
+  // Verifico si viene el parámetro price
+  if(req.query.price) {
+
+    if(req.query.price.match(/^-?\d+$/)) {
+      // tipo number o -number
+      const valor = Number(req.query.price);
+      if(valor < 0) {
+        filter.price = { $lte: Math.abs(valor) }; // Busco los precios menores al indicado
+      } else {
+        filter.price = req.query.price; // Busco el precio exacto
+      }
+    }
+
+    if(req.query.price.match(/^\d+-$/)) {
+      // tipo number-
+      filter.price = { $gte: parseInt(req.query.price) };
+    }
+
+    if(req.query.price.match(/^-?\d+-\d+$/)) {
+      // tipo number-number
+      const priceArray = req.query.price.split('-');
+      filter.price = { $gte: priceArray[0], $lte: priceArray[1] };
+    }
+
+    if(!filter.price) {
+      // si viene el parámetro pero no coincidió con nada entonces viene mal formateado
+      return { error:
+        {
+          code: 400,
+          message: 'Price parameter should be like `int1` or `int1-int2` or `-int1` or `int1-`'
+        }
+      };
+    }
+  }
+
+  const limit = parseInt(req.query.limit || 15);
+  const skip = parseInt(req.query.skip);
+  const sort = req.query.sort;
+
+
   const query = Advertisement.find(filter);
   query.limit(limit);
   query.skip(skip);
